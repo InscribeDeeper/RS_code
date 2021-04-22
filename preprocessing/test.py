3fdcfeb015c61e25e6acaf760adba66672652ce2
WY <hey.weiyang@gmail.com>
2021-04-22 13:30:35 -0400

    Update main code in jupyter file

---
+++ preprocessing/test.py
@@ -0,0 +1,4 @@
+import pandas as pd
+output_folder = "../processed_data/"
+# pd.read_csv(output_folder + 'CF_click/today/user_item_from_start.csv', index_col="user_ID")
+pd.read_csv("../processed_data/CF_click/today/user_item_from_start.csv")
